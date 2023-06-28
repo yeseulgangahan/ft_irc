0d05ff08ada525c5be8cbaf9484130418eeeec75
Jaehoon Shin <jshin@c4r9s5.42seoul.kr>
2023-06-28 17:14:56 +0900

    ADD: rmOverlaps test

--- include/CmdHandler.hpp
+++ include/CmdHandler.hpp
@@ -43,13 +43,13 @@ class CmdHandler
 		bool requireEnoughParams(Client &sender, const Command& cmd, size_t ok_min, size_t ng_min = SIZE_MAX, bool require_trailing = false);
 		
 		std::vector<Command> parseCommands(const std::string &commands_msg);
-		void executeCommand(Client &sender, const Command &cmd);
+		void executeCommand(Client &sender, Command cmd);
 		void cap(Client &client, const Command &cmd);
 		void pass(Client &client, const Command& cmd);
 		void nick(Client &client, const Command& cmd);
 		void user(Client &client, const Command &cmd);
 		void quit(Client&client, const Command &cmd);
-		void mode(Client &client, const Command& cmd);
+		void mode(Client &client, Command& cmd);
 		void privmsg(Client& client, const Command &cmd);
 		void kick(Client &client, const Command& cmd);
 		void invite(Client& sender, const Command& cmd);
@@ -62,6 +62,7 @@ class CmdHandler
 		bool requireAuthed(Client &client);
 		bool requireRegistrationDone(Client &client);
 
+		void rmOverlaps(std::string& s);
 		void plusOption(Channel &channel, Client &sender, const Command &cmd);
 		void minusOption(Channel &channel, Client &sender, const Command &cmd);
 };
